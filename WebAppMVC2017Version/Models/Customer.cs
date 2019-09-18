f38c27b5df3cb8e7b6c1fc53e8c21695f83e2482
Jez Horton <JHorton@spartaglobal.com>
2019-09-18 19:48:30 +0100

    Initial Commit

---
+++ WebAppMVC2017Version/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC2017Version.Models
+{
+    public class Customer
+    {
+        string firstName = "", lastName = "";
+        int age = 0;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
